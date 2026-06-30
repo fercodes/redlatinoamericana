8b2b3c5fdd1f545323476aa88ffbeced0081f0ec
redlatamplanetarias <redlatamcsplanetarias@gmail.com>
2026-06-30 21:52:02 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
+import dotenv from 'dotenv';
+
+// Forzamos a Astro a leer tu archivo personalizado
+dotenv.config({ path: 'env.txt' });
 
 export default defineConfig({
   output: 'server',
